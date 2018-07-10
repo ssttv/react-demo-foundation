abbf10be89804e215900bb567622389c0e375b54
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-11 00:59:45 +0300

    chore: install dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+export default function App() {
+  return (
+    <div className="App">
       <h3>Hello, world</h3>
-      </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
