4904441dec2ab647aa0dfd12d8da273bb5ccc7ba
Sergey Titov <sergey.sergeevich.titov@protonmail.com>
2018-07-11 07:34:08 +0300

    feat: basic todo functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h3>Hello, world</h3>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ""
+    };
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({ newTodo: value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({ todos, newTodo: "" });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input
+            onChange={this.handleChange.bind(this)}
+            value={this.state.newTodo}
+            type="text"
+            placeholder="new todo"
+          />
+          <button onClick={this.handleClick.bind(this)}>create</button>
+        </form>
+        <ul>{this.state.todos.map(todo => <li>{todo}</li>)}</ul>
+      </div>
+    );
+  }
 }
+
+export default App;
